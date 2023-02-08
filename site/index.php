304499b02d88f8d8cd564db26c3c04ea3b4886d5
Tom Alkalai <Tomalkalai0804@gmail.com>
2023-02-08 09:51:13 +0100

    werken met arrays

--- site/index.php
+++ site/index.php
@@ -1,10 +1,27 @@
 <?php
 
-$number = -2346;
+$eenAantalGetallen = [];
 
-if($number > 0){
-    echo "Het getal is positief";
-}
-else{
-    echo "Het getal is Negatief";
-}
+$eenAantalGetallen = [12,66,87,300];
+
+echo $eenAantalGetallen[2];
+
+$eenAantalGetallen =[
+    
+    0 => 12,
+    1 => 66,
+    2 => 87,
+    3 => 300
+];
+
+echo $eenAantalGetallen[3];
+
+$eenWillekeurigLijstje = [
+
+    0 => "Tom",
+    1 => "Alkalai",
+    2 => 18
+];
+
+// print_r($eenWillekeurigLijstje);
+var_dump($eenWillekeurigLijstje);
